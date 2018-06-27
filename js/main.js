35d2c9aa2495697f9e7461c1d8f01327cb1b98d1
camila <camiramirezpp@gmail.com>
2018-06-27 19:17:03 -0400

    html para cada boton creados

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 function enter() {
   const pass = document.getElementById('inputPassword').value;
   if (pass.length <= 8) {
-    const btnLogin = document.getElementById('btnIngresar');
-    btnLogin.addEventListener('click', () => {
-      document.getElementsByClassName('loginUser')[0].classList.add('btnIngresar');
-      document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
-    }
-    );
+    window.open('index.html')
   } else {
     document.getElementById('incorrectPass').innerHTML = 'La contraseña debe contener máximo 8 números.';
   }
