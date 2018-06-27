a6dc214ec73b811ee3906e7391df395151ac0020
camila <camiramirezpp@gmail.com>
2018-06-27 12:52:51 -0400

    login funcionando con display none

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-/* Holiii acá va tu código también */
+function enter() {
+  const pass = document.getElementById('inputPassword').value;
+  if (pass.length <= 8) {
+    const btnLogin = document.getElementById('btnIngresar');
+    btnLogin.addEventListener('click', () => {
+      document.getElementsByClassName('loginUser')[0].classList.add('btnIngresar');
+      document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
+    }
+    );
+  } else {
+    document.getElementById('incorrectPass').innerHTML = 'La contraseña debe contener máximo 8 números.';
+  }
+}
